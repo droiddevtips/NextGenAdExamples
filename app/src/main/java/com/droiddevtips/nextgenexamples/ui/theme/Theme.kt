1d761e42e914b266ba0a4e9fc0846c629f5bf6b9
melchior <vrol0004@gmail.com>
2026-08-01 19:21:23 +0200

    code cleanup

--- app/src/main/java/com/droiddevtips/nextgenexamples/ui/theme/Theme.kt
+++ app/src/main/java/com/droiddevtips/nextgenexamples/ui/theme/Theme.kt
@@ -68,8 +68,6 @@ val themeShapes = Shapes(
 @Composable
 fun DroidDevTipsTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
