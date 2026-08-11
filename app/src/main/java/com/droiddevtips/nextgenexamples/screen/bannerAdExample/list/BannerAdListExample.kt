fea2b99f3182b3bab5aa988597bdbe9cfa491c2f
melchior <vrol0004@gmail.com>
2026-08-12 00:16:46 +0200

    grid list bug fixed and placeholder when no ads is found in progress

--- app/src/main/java/com/droiddevtips/nextgenexamples/screen/bannerAdExample/list/BannerAdListExample.kt
+++ app/src/main/java/com/droiddevtips/nextgenexamples/screen/bannerAdExample/list/BannerAdListExample.kt
@@ -4,27 +4,20 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.droiddevtips.nextgenexamples.screen.bannerAdExample.data.BannerAdExampleDisplayItem
 import com.droiddevtips.nextgenexamples.screen.bannerAdExample.data.BannerAdExampleViewModelAction
 import com.droiddevtips.nextgenexamples.screen.bannerAdExample.data.BannerAdExampleViewState
-import com.droiddevtips.nextgenexamples.screen.bannerAdExample.data.BannerAdExampleDisplayItem
 
 @Composable
 fun BannerAdListExample(
