80d7b7362f595300467284986c26ccf4892421ab
melchior <vrol0004@gmail.com>
2026-08-16 00:27:37 +0200

    NavigatorViewAction cleaned up

--- app/src/main/java/com/droiddevtips/nextgenexamples/navigator/data/NavigatorViewAction.kt
+++ app/src/main/java/com/droiddevtips/nextgenexamples/navigator/data/NavigatorViewAction.kt
@@ -1,5 +1,11 @@
 package com.droiddevtips.nextgenexamples.navigator.data
 
+/**
+ * The actions accepted by the navigator view
+ *
+ * Created by Melchior Vrolijk
+ * Droid Dev Tips (c) 2025. All rights reserved.
+ */
 sealed interface NavigatorViewAction {
     data class SetSelectedItem(val screen: Screen): NavigatorViewAction
 }
