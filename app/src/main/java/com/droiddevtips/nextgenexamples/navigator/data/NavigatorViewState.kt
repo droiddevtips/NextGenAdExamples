681e359101d283fec389f59ad7db499d5fa2e297
melchior <vrol0004@gmail.com>
2026-08-16 00:28:23 +0200

    NavigatorViewState cleaned up

--- app/src/main/java/com/droiddevtips/nextgenexamples/navigator/data/NavigatorViewState.kt
+++ app/src/main/java/com/droiddevtips/nextgenexamples/navigator/data/NavigatorViewState.kt
@@ -3,5 +3,11 @@ package com.droiddevtips.nextgenexamples.navigator.data
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
+/**
+ * Navigator view state data model
+ *
+ * Created by Melchior Vrolijk
+ * Droid Dev Tips (c) 2025. All rights reserved.
+ */
 @Parcelize
 data class NavigatorViewState(val selectedItem: Screen? = null): Parcelable
