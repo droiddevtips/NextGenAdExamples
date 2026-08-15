aa1e89ca9235aebdb3e4bde32758389710535e4b
melchior <vrol0004@gmail.com>
2026-08-16 00:41:03 +0200

    bannerAdTabTitles cleaned up

--- app/src/main/java/com/droiddevtips/nextgenexamples/screen/bannerAdExample/data/BannerAdTabs.kt
+++ app/src/main/java/com/droiddevtips/nextgenexamples/screen/bannerAdExample/data/BannerAdTabs.kt
@@ -4,6 +4,12 @@ import com.droiddevtips.nextgenexamples.core.AppString
 import com.droiddevtips.nextgenexamples.core.Drawable
 import com.droiddevtips.nextgenexamples.core.Tab
 
+/**
+ * The list containing the available tabs for the banner ad example
+ *
+ * Created by Melchior Vrolijk
+ * Droid Dev Tips (c) 2025. All rights reserved.
+ */
 val bannerAdTabTitles = listOf(
     Tab(title = AppString.list, icon = Drawable.list_icon),
     Tab(title = AppString.grid, icon = Drawable.grid_icon)
