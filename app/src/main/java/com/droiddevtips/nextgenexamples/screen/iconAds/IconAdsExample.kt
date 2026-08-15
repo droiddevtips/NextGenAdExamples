9ad8628048c9f798a31c2ebfc98bc336e197914f
melchior <vrol0004@gmail.com>
2026-08-16 00:53:08 +0200

    IconAdsExample cleaned up

--- app/src/main/java/com/droiddevtips/nextgenexamples/screen/iconAds/IconAdsExample.kt
+++ app/src/main/java/com/droiddevtips/nextgenexamples/screen/iconAds/IconAdsExample.kt
@@ -4,6 +4,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.droiddevtips.nextgenexamples.screen.comingPlaceholder.ComingSoonPlaceholder
 
+/**
+ * The icon ads example composable
+ *
+ * Created by Melchior Vrolijk
+ * Droid Dev Tips (c) 2026. All rights reserved.
+ */
 @Composable
 fun IconAdsExample(modifier: Modifier = Modifier) {
 
