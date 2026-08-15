3d75ae489dc3184a616c0466e7baea319495414d
melchior <vrol0004@gmail.com>
2026-08-16 00:53:45 +0200

    InterstitialAdsExample cleaned up

--- app/src/main/java/com/droiddevtips/nextgenexamples/screen/interstitialAds/InterstitialAdsExample.kt
+++ app/src/main/java/com/droiddevtips/nextgenexamples/screen/interstitialAds/InterstitialAdsExample.kt
@@ -4,6 +4,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.droiddevtips.nextgenexamples.screen.comingPlaceholder.ComingSoonPlaceholder
 
+/**
+ * The interstitial ads example composable
+ *
+ * Created by Melchior Vrolijk
+ * Droid Dev Tips (c) 2026. All rights reserved.
+ */
 @Composable
 fun InterstitialAdsExample(modifier: Modifier = Modifier) {
 
