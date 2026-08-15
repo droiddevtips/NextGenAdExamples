c403f23be5f802940ee4f32c65bee04b4f7a1b8d
melchior <vrol0004@gmail.com>
2026-08-16 00:39:00 +0200

    BannerAdExampleViewModelAction cleaned up

--- app/src/main/java/com/droiddevtips/nextgenexamples/screen/bannerAdExample/data/BannerAdExampleViewModelAction.kt
+++ app/src/main/java/com/droiddevtips/nextgenexamples/screen/bannerAdExample/data/BannerAdExampleViewModelAction.kt
@@ -1,7 +1,11 @@
 package com.droiddevtips.nextgenexamples.screen.bannerAdExample.data
 
+/**
+ * The available banner ad example view model actions
+ *
+ * Created by Melchior Vrolijk
+ * Droid Dev Tips (c) 2025. All rights reserved.
+ */
 sealed interface BannerAdExampleViewModelAction {
-
     data object DestroyAllBannerAds: BannerAdExampleViewModelAction
-
 }
