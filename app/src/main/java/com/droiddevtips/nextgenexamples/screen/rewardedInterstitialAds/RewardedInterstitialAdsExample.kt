18c111b62d39490aa98d4bf3532e8a21b55e90fc
melchior <vrol0004@gmail.com>
2026-08-16 00:55:22 +0200

    RewardedInterstitialAdsExample cleaned up

--- app/src/main/java/com/droiddevtips/nextgenexamples/screen/rewardedInterstitialAds/RewardedInterstitialAdsExample.kt
+++ app/src/main/java/com/droiddevtips/nextgenexamples/screen/rewardedInterstitialAds/RewardedInterstitialAdsExample.kt
@@ -4,9 +4,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.droiddevtips.nextgenexamples.screen.comingPlaceholder.ComingSoonPlaceholder
 
+/**
+ * The rewarded interstitial ads example composable
+ *
+ * Created by Melchior Vrolijk
+ * Droid Dev Tips (c) 2026. All rights reserved.
+ */
 @Composable
 fun RewardedInterstitialAdsExample(modifier: Modifier = Modifier) {
-
     ComingSoonPlaceholder(modifier = modifier)
-
 }
