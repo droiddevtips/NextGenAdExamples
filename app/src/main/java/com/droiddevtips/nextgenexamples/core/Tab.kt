72c13b80112cfa951460bba9d50f7ea4c5a286d4
melchior <vrol0004@gmail.com>
2026-08-16 00:14:36 +0200

    Tab and Typealias cleaned up

--- app/src/main/java/com/droiddevtips/nextgenexamples/core/Tab.kt
+++ app/src/main/java/com/droiddevtips/nextgenexamples/core/Tab.kt
@@ -1,3 +1,9 @@
 package com.droiddevtips.nextgenexamples.core
 
+/**
+ * The data model that represent tabs
+ *
+ * Created by Melchior Vrolijk
+ * Droid Dev Tips (c) 2026. All rights reserved.
+ */
 data class Tab(val title: Int, val icon: Int)
--- app/src/main/java/com/droiddevtips/nextgenexamples/core/TypeAlias.kt
+++ app/src/main/java/com/droiddevtips/nextgenexamples/core/TypeAlias.kt
@@ -3,10 +3,11 @@ package com.droiddevtips.nextgenexamples.core
 import com.droiddevtips.nextgenexamples.R
 
 /**
+ * Type aliases for commonly used for generic types.
+ *
  * Created by Melchior Vrolijk
  * Droid Dev Tips (c) 2025. All rights reserved.
  */
-
 typealias AppFont = R.font
 typealias Drawable = R.drawable
 typealias AppString = R.string
