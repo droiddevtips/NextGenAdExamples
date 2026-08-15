cca0e8d5d7e51da447590b0dbe1e771661b0ace1
melchior <vrol0004@gmail.com>
2026-08-16 00:36:36 +0200

    Article cleaned up

--- app/src/main/java/com/droiddevtips/nextgenexamples/screen/bannerAdExample/data/Article.kt
+++ app/src/main/java/com/droiddevtips/nextgenexamples/screen/bannerAdExample/data/Article.kt
@@ -3,5 +3,11 @@ package com.droiddevtips.nextgenexamples.screen.bannerAdExample.data
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
+/**
+ * The article data model
+ *
+ * Created by Melchior Vrolijk
+ * Droid Dev Tips (c) 2025. All rights reserved.
+ */
 @Parcelize
 data class Article(val featureImage:Int, val title:String, val description:String): Parcelable
