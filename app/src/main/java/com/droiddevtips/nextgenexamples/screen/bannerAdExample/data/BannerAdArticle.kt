fa5322570715559280b5efc500e4dd9043cd502c
melchior <vrol0004@gmail.com>
2026-08-16 00:37:02 +0200

    BannerAdArticle cleaned up

--- app/src/main/java/com/droiddevtips/nextgenexamples/screen/bannerAdExample/data/BannerAdArticle.kt
+++ app/src/main/java/com/droiddevtips/nextgenexamples/screen/bannerAdExample/data/BannerAdArticle.kt
@@ -3,6 +3,12 @@ package com.droiddevtips.nextgenexamples.screen.bannerAdExample.data
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
+/**
+ * The banner ad article data model
+ *
+ * Created by Melchior Vrolijk
+ * Droid Dev Tips (c) 2025. All rights reserved.
+ */
 @Parcelize
 data class BannerAdArticle(
     val id: Int,
