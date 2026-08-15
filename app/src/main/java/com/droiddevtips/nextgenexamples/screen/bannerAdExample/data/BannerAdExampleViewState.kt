5dd23a61aabb9d692188775de102deaf5349a2b6
melchior <vrol0004@gmail.com>
2026-08-16 00:39:44 +0200

    BannerAdExampleViewState cleaned up

--- app/src/main/java/com/droiddevtips/nextgenexamples/screen/bannerAdExample/data/BannerAdExampleViewState.kt
+++ app/src/main/java/com/droiddevtips/nextgenexamples/screen/bannerAdExample/data/BannerAdExampleViewState.kt
@@ -3,6 +3,12 @@ package com.droiddevtips.nextgenexamples.screen.bannerAdExample.data
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
+/**
+ * The banner ad example view state data model
+ *
+ * Created by Melchior Vrolijk
+ * Droid Dev Tips (c) 2025. All rights reserved.
+ */
 @Parcelize
 data class BannerAdExampleViewState(
     val isLoading: Boolean = true,
