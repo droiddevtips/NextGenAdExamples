a5016ff81b2a3f40dd61ba48d4ee108b4759240d
melchior <vrol0004@gmail.com>
2026-08-16 00:21:33 +0200

    OnConsentGatheringCompleteListener cleaned up

--- app/src/main/java/com/droiddevtips/nextgenexamples/googleAdsConsentManager/OnConsentGatheringCompleteListener.kt
+++ app/src/main/java/com/droiddevtips/nextgenexamples/googleAdsConsentManager/OnConsentGatheringCompleteListener.kt
@@ -2,6 +2,18 @@ package com.droiddevtips.nextgenexamples.googleAdsConsentManager
 
 import com.google.android.ump.FormError
 
+/**
+ * Listener invoked when the consent gathering process has completed.
+ *
+ * This callback fires once the User Messaging Platform (UMP) SDK has finished
+ * requesting the latest consent information and, if required, has presented
+ * and dismissed a consent form to the user. It signals that the app can now
+ * safely check the user's consent status and proceed with SDK initialization
+ * (e.g. initializing the Google Mobile Ads SDK) or ad loading.
+ *
+ * Created by Melchior Vrolijk
+ * Droid Dev Tips (c) 2025. All rights reserved.
+ */
 interface OnConsentGatheringCompleteListener {
 
     fun consentGatheringComplete(error: FormError?)
