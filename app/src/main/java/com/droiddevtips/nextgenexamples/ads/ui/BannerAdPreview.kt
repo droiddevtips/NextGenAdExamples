b49bca9f6db33ce0c72be29006acd096801db850
melchior <vrol0004@gmail.com>
2026-08-16 00:06:40 +0200

    BannerAdPreview cleaned up

--- app/src/main/java/com/droiddevtips/nextgenexamples/ads/ui/BannerAdPreview.kt
+++ app/src/main/java/com/droiddevtips/nextgenexamples/ads/ui/BannerAdPreview.kt
@@ -3,14 +3,22 @@ package com.droiddevtips.nextgenexamples.ads.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.droiddevtips.nextgenexamples.core.AppString
 
+/**
+ * Displays a placeholder representation of a banner ad for use in Compose previews.
+ *
+ * Created by Melchior Vrolijk
+ * Droid Dev Tips (c) 2026. All rights reserved.
+ */
 @Composable
 fun BannerAdPreview(modifier: Modifier = Modifier) {
 
@@ -19,10 +27,10 @@ fun BannerAdPreview(modifier: Modifier = Modifier) {
             modifier = Modifier
                 .align(alignment = Alignment.Center)
                 .size(300.dp)
-                .background(color = Color.Red)
+                .background(color = MaterialTheme.colorScheme.secondaryContainer)
         ) {
             Text(
-                text = "Preview",
+                text = stringResource(AppString.preview),
                 textAlign = TextAlign.Center,
                 modifier = Modifier.align(alignment = Alignment.Center)
             )
