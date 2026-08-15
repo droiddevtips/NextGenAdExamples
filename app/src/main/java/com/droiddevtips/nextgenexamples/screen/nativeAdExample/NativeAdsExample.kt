f2f0029cf37f47bbe1f7c1e0b95290a42633f384
melchior <vrol0004@gmail.com>
2026-08-16 00:54:27 +0200

    NativeAdExample cleaned up

--- app/src/main/java/com/droiddevtips/nextgenexamples/screen/nativeAdExample/NativeAdsExample.kt
+++ app/src/main/java/com/droiddevtips/nextgenexamples/screen/nativeAdExample/NativeAdsExample.kt
@@ -4,10 +4,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.droiddevtips.nextgenexamples.screen.comingPlaceholder.ComingSoonPlaceholder
 
+/**
+ * The native ad example composable
+ *
+ * Created by Melchior Vrolijk
+ * Droid Dev Tips (c) 2026. All rights reserved.
+ */
 @Composable
 fun NativeAdExample(modifier: Modifier = Modifier) {
-
     ComingSoonPlaceholder(modifier = modifier)
-
-
 }
