9907b38b540d59e5c1a89f4bd9b994d2f005650b
melchior <vrol0004@gmail.com>
2026-08-23 22:21:49 +0200

    missing interstitial view model factory added

---
+++ app/src/main/java/com/droiddevtips/nextgenexamples/screen/interstitialAds/ui/InterstitialAdsExampleViewModelFactory.kt
@@ -0,0 +1,31 @@
+@file:Suppress("UNCHECKED_CAST")
+
+package com.droiddevtips.nextgenexamples.screen.interstitialAds.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import com.droiddevtips.nextgenexamples.ads.data.manager.AppAdManager
+
+/**
+ * Factory for creating instances of [InterstitialAdsExampleViewModel].
+ *
+ * Since [InterstitialAdsExampleViewModel] requires dependencies (such as [AdManager])
+ * that cannot be resolved by the default no-argument [ViewModelProvider.Factory],
+ * this factory manually constructs the ViewModel and injects the required
+ * dependencies.
+ *
+ * Created by Melchior Vrolijk
+ * Droid Dev Tips (c) 2026. All rights reserved.
+ */
+class InterstitialAdsExampleViewModelFactory : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
+
+        if (modelClass.isAssignableFrom(InterstitialAdsExampleViewModel::class.java)) {
+            return InterstitialAdsExampleViewModel(adManager = AppAdManager) as T
+        }
+
+        throw IllegalArgumentException("Not ${InterstitialAdsExampleViewModel::class.simpleName} class")
+    }
+}
